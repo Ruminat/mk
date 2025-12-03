5820ab0ed49cd25bd46c692a7d3e28e338b1526e
Vlad Furman <Blowlyge@gmail.com>
2025-12-03 16:38:58 +0300

    fixed throwing errors

--- apps/server/src/modules/mood/service.ts
+++ apps/server/src/modules/mood/service.ts
@@ -8,7 +8,7 @@ export const moodService = {
     const response = await db.insert(MoodTable).values(entry).returning();
 
     if (response.length !== 1) {
-      new ServiceError("Failed to add mood entry");
+      throw new ServiceError("Failed to add mood entry");
     }
 
     const [row] = response;
@@ -17,15 +17,20 @@ export const moodService = {
   },
 
   deleteMoodEntry: async (entryId: TSelectMoodEntry["id"]) => {
-    const response = await db.delete(MoodTable).where(eq(MoodTable.id, entryId));
+    const response = await db
+      .delete(MoodTable)
+      .where(eq(MoodTable.id, entryId));
 
-    if (response.rows.length !== 1) {
-      new ServiceError("Failed to delete the mood entry");
+    if (response.rowsAffected !== 1) {
+      throw new ServiceError("Failed to delete the mood entry");
     }
   },
 
   listMoodEntries: async ({ userId }: Pick<TSelectMoodEntry, "userId">) => {
-    const response = await db.select().from(MoodTable).where(eq(MoodTable.userId, userId));
+    const response = await db
+      .select()
+      .from(MoodTable)
+      .where(eq(MoodTable.userId, userId));
 
     return response;
   },
